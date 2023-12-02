9e90fe2758183be529006906bea042b91d93ae21
Adelina Fatu <adelinamfatu@gmail.com>
2023-12-02 15:42:50 +0200

    Added vuetify and vue 3 frontend app

---
+++ client/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card>
+          <v-card-title class="text-h5">Login</v-card-title>
+          <v-card-text>
+            <v-form @submit.prevent="login">
+              <v-text-field v-model="username" label="Username"></v-text-field>
+              <v-text-field v-model="password" label="Password" type="password"></v-text-field>
+              <v-btn type="submit" color="primary">Login</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
