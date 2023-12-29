eb3a53769fa4280f4a97f4da30eebf904719b7a7
Adelina Fatu <adelinamfatu@gmail.com>
2023-12-29 21:25:36 +0200

    Added logout dialog and removal of token from cookies

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -6,6 +6,16 @@
         rail
         color="blue-lighten-4"
       >
+      <v-dialog v-model="dialogLogout" max-width="400px">
+        <v-card>
+        <v-card-title class="text-center">Confirm Logout</v-card-title>
+        <v-card-text class="text-center">Are you sure you want to logout?</v-card-text>
+        <v-card-actions class="justify-center">
+          <v-btn text @click="dialogLogout = false">No</v-btn>
+          <v-btn text color="primary" @click="logout">Yes</v-btn>
+        </v-card-actions>
+        </v-card>
+      </v-dialog>
         <v-list>
           <v-list-item
             prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
@@ -18,9 +28,25 @@
           <v-list-item prepend-icon="mdi-bookshelf" title="My bookshelves" value="myfiles" class="mt-5"></v-list-item>
           <v-list-item prepend-icon="mdi-star" title="Starred" value="starred" class="mt-5"></v-list-item>
           <v-list-item prepend-icon="mdi-heart" title="Shared with me" value="shared" class="mt-5"></v-list-item>
+          <v-list-item prepend-icon="mdi-logout" title="Logout" value="logout" class="mt-10" @click="showLogoutDialog"></v-list-item>
         </v-list>
       </v-navigation-drawer>
       <v-main style="height: 85vh"></v-main>
     </v-layout>
   </v-card>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const dialogLogout = ref(false);
+
+const showLogoutDialog = () => {
+  dialogLogout.value = true;
+}
+
+const logout = () => {
+  localStorage.removeItem('firebaseToken');
+  dialogLogout.value = false;
+}
+</script>
