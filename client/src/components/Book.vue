886102c5fc7b63adb72221a3687438cbf42189b6
Adelina Fatu <adelinamfatu@gmail.com>
2024-01-12 20:47:29 +0200

    Added v-slider with hardcoded values

--- client/src/components/Book.vue
+++ client/src/components/Book.vue
@@ -1,6 +1,15 @@
 <template>
-    <v-card>
-      <v-card-title>Book 1</v-card-title>
-      <v-card-text>By me.</v-card-text>
-    </v-card>
+  <v-card hover elevation="5" style="width: 8rem; height: 11rem">
+    <v-img src="@/assets/shatterme.jpg" height="120" contain></v-img>
+    <v-card-title align="center" style="font-size: 16px; font-weight: bold; margin: 0; padding: 0;">{{ book.title }}</v-card-title>
+    <v-card-subtitle align="center" style="font-size: 12px; font-weight: italic; margin: 0;">{{ book.author }}</v-card-subtitle>
+  </v-card>
 </template>
+
+<script>
+export default {
+  props: {
+    book: Object,
+  },
+};
+</script>
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -3,7 +3,25 @@
     <v-card style="height: 95vh; width: 100%" elevation="12">
       <v-card-title>Dashboard</v-card-title>
       <v-card-text>
-        <p>This is the dashboard.</p>
+        <p>Most popular this week</p>
+        <v-slide-group
+          v-model="model"
+          class="pa-4"
+          selected-class="bg-success"
+          show-arrows
+        >
+          <v-slide-group-item
+            v-for="(book, index) in books"
+            :key="index"
+            v-slot="{ toggle, selectedClass }"
+          >
+            <book
+              :book="book"
+              :class="['ma-4', selectedClass]"
+              @click="toggle"
+            ></book>
+          </v-slide-group-item>
+        </v-slide-group>
       </v-card-text>
     </v-card>
   </div>
@@ -12,15 +30,29 @@
 <script>
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router'
+import Book from '@/components/Book.vue';
 
 export default {
   name: 'Dashboard',
+  components: {
+    Book,
+  },
   setup() {
     const store = useStore();
     const router = useRouter();
+    /*
     if (!store.state.user) {
         router.push('/login');
-    }
+    }*/
+  },
+  data() {
+    return {
+      model: [],
+      books: [
+        { title: 'Book 1', author: 'Author 1' },
+        { title: 'Book 2', author: 'Author 2' },
+      ],
+    };
   },
 };
 </script>
