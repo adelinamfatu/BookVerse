a7e840228551e934d704bb9ab6313e2575b5721c
Adelina Fatu <adelinamfatu@gmail.com>
2024-01-13 16:27:04 +0200

    Added navigation back from the book details page to dashboard

--- client/src/components/BookDetails.vue
+++ client/src/components/BookDetails.vue
@@ -1,5 +1,31 @@
 <template>
-    <v-card>
-        <h2>Hello</h2>
+  <div class="center-container">
+    <v-card height="80vh" width="80vw" elevation="10" color="blue-grey-lighten-5">
+      <v-card-actions>
+        <v-btn @click="goBack"> 
+          <v-icon>mdi-arrow-left</v-icon> Return
+        </v-btn>
+      </v-card-actions>
     </v-card>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.center-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #E3F2FD;
+}
+</style>
